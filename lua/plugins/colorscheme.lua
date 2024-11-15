84a2134c17d815892525d86319e0c56a9ab944e4
mamatov-ilich <mamatovilich@gmail.com>
2024-11-16 04:51:52 +0600

    lazy config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  "craftzdog/solarized-osaka.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+      enable_italic = "1",
+    }
+  end,
+}
